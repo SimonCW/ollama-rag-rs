de6b8364f163ad62d62d1d54b89ecd6c4543c67a
Simon Weiß <weiss.simon@outlook.com>
2024-01-15 11:48:46 +0100

    Initial commit, copy from ex2

---
+++ examples/ex3-chat.rs
@@ -0,0 +1,42 @@
+use std::io::stdout;
+
+use futures::StreamExt;
+use ollama_rs::generation::completion::GenerationContext;
+use rag_rs::consts::{MODEL, SYSTEM_DEFAULT};
+use rag_rs::gen::write_stream;
+
+use anyhow::{anyhow, Context, Result};
+use ollama_rs::{generation::completion::request::GenerationRequest, Ollama};
+use tokio::io::AsyncWriteExt;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // localhost:1143
+    let ollama = Ollama::default();
+
+    let prompts = &[
+        "What's the capital of France",
+        "What is the capital of Germany?",
+        "What was my first question?",
+    ];
+
+    let mut last_ctx: Option<GenerationContext> = None;
+    for prompt in prompts {
+        let mut gen_req = GenerationRequest::new(MODEL.to_string(), prompt.to_string())
+            .system(SYSTEM_DEFAULT.to_string());
+        if let Some(ctx) = last_ctx.take() {
+            gen_req = gen_req.context(ctx);
+        }
+        println!(">>> {prompt}");
+        let final_data = write_stream(&ollama, gen_req).await?;
+        if let Some(data) = final_data {
+            last_ctx = Some(data.context);
+        }
+    }
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+}
