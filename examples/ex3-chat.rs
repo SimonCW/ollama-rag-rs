63955d15346e9835369a07ef0ff7d24e8c0168fb
Simon Weiß <weiss.simon@outlook.com>
2024-01-17 17:52:29 +0100

    Improved user assistant chat

--- examples/ex3-chat.rs
+++ examples/ex3-chat.rs
@@ -1,6 +1,8 @@
 use std::io::stdout;
 
 use futures::StreamExt;
+use ollama_rs::generation::chat::request::ChatMessageRequest;
+use ollama_rs::generation::chat::{ChatMessage, MessageRole};
 use ollama_rs::generation::completion::GenerationContext;
 use rag_rs::consts::{MODEL, SYSTEM_DEFAULT};
 use rag_rs::gen::write_stream;
@@ -14,29 +16,83 @@ async fn main() -> Result<()> {
     // localhost:1143
     let ollama = Ollama::default();
 
-    let prompts = &[
+    let user_msgs = &[
         "What's the capital of France",
         "What is the capital of Germany?",
         "What was my first question?",
     ];
+    let system_msg = ChatMessage::new(MessageRole::System, SYSTEM_DEFAULT.to_string());
+    let mut msg_thread: Vec<ChatMessage> = vec![system_msg];
 
-    let mut last_ctx: Option<GenerationContext> = None;
-    for prompt in prompts {
-        let mut gen_req = GenerationRequest::new(MODEL.to_string(), prompt.to_string())
-            .system(SYSTEM_DEFAULT.to_string());
-        if let Some(ctx) = last_ctx.take() {
-            gen_req = gen_req.context(ctx);
-        }
-        println!(">>> {prompt}");
-        let final_data = write_stream(&ollama, gen_req).await?;
-        if let Some(data) = final_data {
-            last_ctx = Some(data.context);
+    for user_msg in user_msgs {
+        println!(">> User: {user_msg}");
+        let user_msg = ChatMessage::new(MessageRole::User, user_msg.to_string());
+        msg_thread.push(user_msg);
+        // Clone really necessary?
+        let req = ChatMessageRequest::new(MODEL.to_string(), msg_thread.clone());
+
+        let assistant_msg = write_chat(&ollama, req).await?; // could be a union of response and final object.
+        if let Some(assistant_msg) = assistant_msg {
+            msg_thread.push(assistant_msg)
         }
     }
+    println!("{msg_thread:#?}");
     Ok(())
+}
+
+pub async fn write_chat(
+    ollama: &Ollama,
+    chat_req: ChatMessageRequest,
+) -> Result<Option<ChatMessage>> {
+    let mut stream = ollama.send_chat_messages_stream(chat_req).await?;
+    let mut stdout = tokio::io::stdout();
+    let mut char_count = 0;
+    let mut msg_stream: Vec<String> = Vec::new();
+
+    while let Some(res) = stream.next().await {
+        let res = res.map_err(|e| anyhow!("stream.next error: {:#?}", e))?;
+
+        if let Some(msg) = res.message {
+            let msg_content = msg.content;
+            // Poor man's wrapping
+            char_count += msg_content.len();
+            if char_count > 80 {
+                stdout.write_all(b"\n").await?;
+                char_count = 0;
+            }
+            // Write output
+            stdout.write_all(msg_content.as_bytes()).await?;
+            stdout.flush().await?;
+            msg_stream.push(msg_content);
+        }
+
+        if let Some(_final_res) = res.final_data {
+            stdout.write_all(b"\n").await?;
+            stdout.flush().await?;
+
+            let assistant_msg = msg_stream.join("");
+            let assistant_msg = ChatMessage::new(MessageRole::Assistant, assistant_msg);
+            return Ok(Some(assistant_msg));
+        }
+        // What if final result never comes, then I'm stuck in endless loop?
+    }
+
+    // new line
+    stdout.write_all(b"\n").await?;
+    stdout.flush().await?;
+
+    Ok(None)
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn test_playground() {
+        let mut v: Vec<i32> = Vec::new();
+        v.push(1);
+        v.push(2);
+        println!("{v:#?}");
+    }
 }
