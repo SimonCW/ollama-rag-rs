77a4f6c71cdbb6d35a9f4e97b6ca4a57c4956fa0
Simon Weiß <weiss.simon@outlook.com>
2024-01-31 16:43:20 +0100

    Initial commit

--- examples/ex6-batch-embedding.rs
+++ examples/ex6-batch-embedding.rs
@@ -1,29 +1,38 @@
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use fastembed::{EmbeddingBase, EmbeddingModel, FlagEmbedding, InitOptions};
+use rag_rs::utils::{ensure_dir, write_vec_to_json};
+use std::{fs, path::Path};
+use text_splitter::TextSplitter;
+use tokenizers::tokenizer::Tokenizer;
+
+const DOCUMENTS_PATH: &str = "./examples/.data/rust_book.txt";
+const EMBEDDINGS_PATH: &str = "./examples/.embeddings/";
 
 pub fn main() -> Result<()> {
-    // With default InitOptions
-    let model: FlagEmbedding = FlagEmbedding::try_new(Default::default())?;
+    let documents_path = Path::new(DOCUMENTS_PATH);
+    let embeddings_path = Path::new(EMBEDDINGS_PATH);
+    ensure_dir(documents_path);
+    ensure_dir(embeddings_path);
+    // Splitting
+    let tokenizer = Tokenizer::from_pretrained("bert-base-cased", None).unwrap();
+    let max_tokens = 1000;
+    let splitter = TextSplitter::new(tokenizer).with_trim_chunks(true);
 
-    // With custom InitOptions
+    let content = fs::read_to_string(documents_path)?;
+    let chunks: Vec<_> = splitter.chunks(&content, max_tokens).collect();
+
+    // Embeddings
     let model: FlagEmbedding = FlagEmbedding::try_new(InitOptions {
         model_name: EmbeddingModel::BGEBaseEN,
         show_download_message: true,
         ..Default::default()
     })?;
 
-    let documents = vec![
-        "passage: Hello, World!",
-        "query: Hello, World!",
-        "passage: This is an example passage.",
-        // You can leave out the prefix but it's recommended
-        "fastembed-rs is licensed under Apache  2.0",
-    ];
+    let embeddings = model.embed(chunks, None)?;
 
-    // Generate embeddings with the default batch size, 256
-    let embeddings = model.embed(documents, None)?;
+    let output_path = embeddings_path.join("rust_book_embeddings.json");
+    println!("Writing embeddings to {}", output_path.display());
+    write_vec_to_json(&output_path, &embeddings);
 
-    println!("Embeddings length: {}", embeddings.len()); // -> Embeddings length: 4
-    println!("Embedding dimension: {}", embeddings[0].len()); // -> Embedding dimension: 768
     Ok(())
 }
