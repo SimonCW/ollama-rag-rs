eb5c75c74c0480a1066d9e2dc8c1457d0e20f55f
Simon Weiß <weiss.simon@outlook.com>
2024-02-01 09:59:18 +0100

    Fix handling of stream output due to breaking change

--- src/lib.rs
+++ src/lib.rs
@@ -31,21 +31,35 @@ pub mod gen {
         let mut stdout = tokio::io::stdout();
         let mut char_count = 0;
 
+        /*
+              while let Some(Ok(res)) = stream.next().await {
+             for ele in res {
+                 stdout.write_all(ele.response.as_bytes()).await?;
+                 stdout.flush().await?;
+
+                 if let Some(final_data) = ele.final_data {
+                     context = Some(final_data.context);
+                 }
+
+        */
+
         while let Some(res) = stream.next().await {
             let res = res.map_err(|e| anyhow!("Error while streaming the next token: {e:#?}"))?;
-            let bytes = res.response.as_bytes();
-            char_count += bytes.len();
-            if char_count > 80 {
-                stdout.write_all(b"\n").await?;
-                char_count = 0;
-            };
-            stdout.write_all(bytes).await?;
-            stdout.flush().await?;
-
-            if let Some(final_data) = res.final_data {
-                stdout.write_all(b"\n---------------\n").await?;
+            for element in res {
+                let bytes = element.response.as_bytes();
+                char_count += bytes.len();
+                if char_count > 80 {
+                    stdout.write_all(b"\n").await?;
+                    char_count = 0;
+                };
+                stdout.write_all(bytes).await?;
                 stdout.flush().await?;
-                return Ok(Some(final_data));
+
+                if let Some(final_data) = element.final_data {
+                    stdout.write_all(b"\n---------------\n").await?;
+                    stdout.flush().await?;
+                    return Ok(Some(final_data));
+                }
             }
         }
         stdout.write_all(b"\n---------------\n").await?;
