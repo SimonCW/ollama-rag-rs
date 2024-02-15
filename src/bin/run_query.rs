6fc301c80539e94af00a9310b92b4853326e1685
Simon Weiß <weiss.simon@outlook.com>
2024-02-15 14:27:09 +0100

    Initial commit

---
+++ src/bin/run_query.rs
@@ -0,0 +1,9 @@
+use anyhow::Result;
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    todo!("Embed query");
+    todo!("Retrieve neighbors");
+    todo!("Create prompt");
+    todo!("Chat with the user");
+}
