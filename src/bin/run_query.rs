31cf9dfb47f8b5c967b8db15f326b818ca4b04b7
Simon Weiß <weiss.simon@outlook.com>
2024-03-04 12:59:14 +0100

    Embed query

--- src/bin/run_query.rs
+++ src/bin/run_query.rs
@@ -1,9 +1,24 @@
 use anyhow::Result;
+use dotenv::dotenv;
+use fastembed::{EmbeddingBase, EmbeddingModel, FlagEmbedding, InitOptions};
+use futures::TryStreamExt;
+use pgvector::Vector;
+use rag_rs::embed::init_model;
+use sqlx::{postgres::PgPoolOptions, Row};
+use std::env;
+use tracing::{info, info_span};
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    todo!("Embed query");
-    todo!("Retrieve neighbors");
+    // Setup
+    dotenv().ok();
+    let model = init_model()?;
+    let db_url = env::var("DATABASE_URL").expect("Environment var DATABASE_URL must be set");
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_url)
+        .await?;
     todo!("Create prompt");
     todo!("Chat with the user");
 }
