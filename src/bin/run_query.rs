5469775ccd254d35382998a2235c810860ced221
Simon Weiß <weiss.simon@outlook.com>
2024-03-04 12:59:26 +0100

    Retrieve neighbors

--- src/bin/run_query.rs
+++ src/bin/run_query.rs
@@ -19,6 +19,31 @@ async fn main() -> Result<()> {
         .max_connections(5)
         .connect(&db_url)
         .await?;
+
+    let query = "What's the 'interior mutability' about and how to achieve it?";
+    println!("Query: {query} \n");
+    let query_embedding = Vector::from(model.query_embed(query)?);
+
+    todo!("Refactor neighbor retrieval")
+    let mut rows = sqlx::query("SELECT id, chunk FROM rippy ORDER BY embedding <-> $1 LIMIT 2")
+        .bind(query_embedding)
+        .fetch(&pool);
+    while let Some(row) = rows.try_next().await? {
+        let neighbor = NearestNeighbors {
+            id: row.get("id"),
+            chunk: row.get("chunk"),
+        };
+        println!(
+            "Neighbor {}:\n {}\n--------------------------------------------------------------",
+            neighbor.id, neighbor.chunk
+        );
+    }
     todo!("Create prompt");
     todo!("Chat with the user");
+}
+
+#[derive(Debug)]
+struct NearestNeighbors {
+    id: i64,
+    chunk: String,
 }
