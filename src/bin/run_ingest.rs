112b57e2023f1f65723c0c231d777db38b81c623
Simon Weiß <weiss.simon@outlook.com>
2024-03-21 16:48:32 +0100

    Use lancedb directly for now

--- src/bin/run_ingest.rs
+++ src/bin/run_ingest.rs
@@ -1,8 +1,12 @@
 use anyhow::{anyhow, Context, Result};
+use arrow_array::{
+    types::Float32Type, FixedSizeListArray, Int32Array, RecordBatch, RecordBatchIterator,
+};
 use dotenv::dotenv;
 use fastembed::{EmbeddingBase, EmbeddingModel, FlagEmbedding};
-use rag_rs::consts::{DOCUMENTS_PATH, MAX_TOKENS};
+use rag_rs::consts::{DOCUMENTS_PATH, EMBEDDINGSIZE, MAX_TOKENS};
 use rag_rs::embed::{init_model, init_splitter};
+use rag_rs::embeddingsdb::{self, Client};
 use rag_rs::utils::ensure_dir;
 use std::env;
 use std::sync::Arc;
@@ -27,25 +31,34 @@ async fn main() -> Result<()> {
     let model = init_model()?;
 
     // Init LanceDB
-    let db_path = env::var("DATABASE_PATH").expect("Environment var DATABASE_PATH must be set");
-    let db = lancedb::connect(&db_path).execute().await.unwrap();
+    let default_conf = embeddingsdb::Config::default();
+    let db_uri = env::var("DATABASE_PATH").expect("Environment var DATABASE_PATH must be set");
+    let conn = lancedb::connect(&db_uri).execute().await?;
+    let tbl = conn.create_empty_table(default_conf.table_name, default_conf.schema);
 
     // Well ... this could be better ;)
     let content = fs::read_to_string(documents_path)?;
     let chunks: Vec<_> = splitter.chunks(&content, MAX_TOKENS).collect();
+    let n_items = chunks.len();
     // Not happy with the clone. How expensive is a clone of a Vec<&str>?
     info!("Creating embeddings");
     let embeddings = model.passage_embed(chunks.clone(), None)?;
+    assert_eq!(embeddings.len(), n_items);
     info!("Inserting embeddings");
-    for (chunk, embedding) in chunks.iter().zip(embeddings) {
-        let embedding = Vector::from(embedding);
-        sqlx::query("INSERT INTO rippy (chunk, embedding) VALUES ($1,$2)")
-            .bind(chunk)
-            .bind(embedding)
-            .execute(&pool)
-            .await
-            .with_context(|| format!("Failed for chunk: '{chunk}'"))?;
-    }
+    let columns = vec![
+        Arc::new(Int32Array::from_iter_values(1..n_items as i32)),
+        Arc::new(
+            FixedSizeListArray::from_iter_primitive::<Float32Type, _, _>(
+                embeddings
+                    .into_iter()
+                    .map(|inner_vec| Some(inner_vec.into_iter().map(|item| Some(item)).collect()))
+                    .collect(),
+                EMBEDDINGSIZE,
+            ),
+        ),
+    ];
+    //vec![RecordBatch::try_new(default_conf.schema, columns)];
+
     info!("Finished inserting embeddings");
     Ok(())
 }
