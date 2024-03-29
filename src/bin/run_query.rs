edafad965a18042602f04bce505fd841e9fd3a00
Simon Weiß <weiss.simon@outlook.com>
2024-03-29 17:24:47 +0100

    Migrate to lancedb

--- src/bin/run_query.rs
+++ src/bin/run_query.rs
@@ -1,43 +1,38 @@
-use anyhow::Result;
+use anyhow::{Context, Result};
 use dotenv::dotenv;
 use fastembed::{EmbeddingBase, EmbeddingModel, FlagEmbedding, InitOptions};
 use futures::TryStreamExt;
-use pgvector::Vector;
-use rag_rs::embed::init_model;
-use sqlx::{postgres::PgPoolOptions, Row};
+use lancedb::query::ExecutableQuery;
+use rag_rs::{consts::EMBEDDING_MODEL, embed::init_model};
 use std::env;
 use tracing::{info, info_span};
 use tracing_subscriber::{fmt, prelude::*, EnvFilter};
+
+const TABLE_NAME: &str = "EmbeddingsTable";
 
 #[tokio::main]
 async fn main() -> Result<()> {
     // Setup;
     dotenv().ok();
     let model = init_model()?;
-    let db_url = env::var("DATABASE_URL").expect("Environment var DATABASE_URL must be set");
-    let pool = PgPoolOptions::new()
-        .max_connections(5)
-        .connect(&db_url)
-        .await?;
+    let db_uri = env::var("DATABASE_PATH").expect("Environment var DATABASE_PATH must be set");
+    let conn = lancedb::connect(&db_uri).execute().await?;
+    let tbl = conn.open_table(TABLE_NAME).execute().await?;
 
     let query = "What's the 'interior mutability' about and how to achieve it?";
     println!("Query: {query} \n");
-    let query_embedding = Vector::from(model.query_embed(query)?);
+    let query_embedding = model.query_embed(query)?;
 
-    todo!("Refactor neighbor retrieval");
-    let mut rows = sqlx::query("SELECT id, chunk FROM rippy ORDER BY embedding <-> $1 LIMIT 2")
-        .bind(query_embedding)
-        .fetch(&pool);
-    while let Some(row) = rows.try_next().await? {
-        let neighbor = NearestNeighbors {
-            id: row.get("id"),
-            chunk: row.get("chunk"),
-        };
-        println!(
-            "Neighbor {}:\n {}\n--------------------------------------------------------------",
-            neighbor.id, neighbor.chunk
-        );
-    }
+    let nearest_neighbors = tbl
+        .query()
+        .nearest_to(query_embedding)
+        .context("Probably cannot convert input vector")?
+        .execute()
+        .await?
+        .try_collect::<Vec<_>>()
+        .await?;
+    println!("{:#?}", nearest_neighbors);
+
     todo!("Create prompt");
     todo!("Chat with the user");
 }
