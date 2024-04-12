d81b6864c5582fced8b3185cd5a65c722c6c3009
Simon Weiß <weiss.simon@outlook.com>
2024-04-12 14:45:02 +0200

    Make simple request

--- src/bin/run_query.rs
+++ src/bin/run_query.rs
@@ -1,4 +1,5 @@
 use anyhow::{Context, Result};
+use arrow_array::StringArray;
 use async_openai::{
     config::OpenAIConfig,
     types::{
@@ -21,16 +22,21 @@ async fn main() -> Result<()> {
     // Setup
     dotenv().ok();
     let model = init_model()?;
+
     let db_uri = env::var("DATABASE_PATH").expect("Environment var DATABASE_PATH must be set");
     let conn = lancedb::connect(&db_uri).execute().await?;
     let tbl = conn.open_table(TABLE_NAME).execute().await?;
 
+    let local_conf = OpenAIConfig::new()
+        .with_api_key("sk-no-key-required")
+        .with_api_base("http://localhost:8080/v1");
+    let client = Client::with_config(local_conf);
+
     // Query
     let query = "What's the 'interior mutability' about and how to achieve it?";
     println!("Query: {query} \n");
+    // Retrieve neighbors
     let query_embedding = model.query_embed(query)?;
-
-    // Setup;
     let nearest_neighbors = tbl
         .query()
         .nearest_to(query_embedding)
@@ -44,39 +50,42 @@ async fn main() -> Result<()> {
         nearest_neighbors.len(),
         "Pretty sure this will always be one ... but not certain"
     );
-    let nearest_neighbors = &nearest_neighbors[0];
-    let local_conf = OpenAIConfig::new()
-        .with_api_key("sk-no-key-required")
-        .with_api_base("http://127.0.0.1:8080");
-    let client = Client::with_config(local_conf);
+    let nn = &nearest_neighbors[0];
+    let nn_chunks = nn
+        .column_by_name("text")
+        .unwrap()
+        .as_any()
+        .downcast_ref::<StringArray>()
+        .unwrap();
+    println!("___________");
+    println!("{:#?}", nn_chunks);
+
+    // Chat
+    let system = ChatCompletionRequestSystemMessageArgs::default()
+                .content("Use the provided CONTEXT to answer questions. If the answer cannot be found in the CONTEXT, write 'I could not find an answer.'")
+                .build()?;
+    let context = "hello";
+    let user_msg = ChatCompletionRequestUserMessageArgs::default()
+        .content(format!(
+            "Use the provided CONTEXT to answer the QUESTION. 
+            QUESTION: {query}
+            CONTEXT: {context}"
+        ))
+        .build()?;
 
     let request = CreateChatCompletionRequestArgs::default()
         .max_tokens(512u16)
-        .model("LLaMA_CPP")
-        .messages([
-            ChatCompletionRequestSystemMessageArgs::default()
-                .content("You are a helpful assistant.")
-                .build()?
-                .into(),
-            ChatCompletionRequestUserMessageArgs::default()
-                .content("Who won the world series in 2020?")
-                .build()?
-                .into(),
-            ChatCompletionRequestAssistantMessageArgs::default()
-                .content("The Los Angeles Dodgers won the World Series in 2020.")
-                .build()?
-                .into(),
-            ChatCompletionRequestUserMessageArgs::default()
-                .content("Where was it played?")
-                .build()?
-                .into(),
-        ])
+        .messages([system.into(), user_msg.into()])
         .build()
         .context("Failed to build ChatCompletionRequest")?;
 
     println!("{}", serde_json::to_string(&request).unwrap());
 
-    let response = client.chat().create(request).await?;
+    let response = client
+        .chat()
+        .create(request)
+        .await
+        .context("Failed to create CompletionResponse")?;
 
     println!("\nResponse:\n");
     for choice in response.choices {
