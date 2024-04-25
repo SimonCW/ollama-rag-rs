59d282b6358a46b3518395ed4150f933cdbc50fd
Simon Weiß <weiss.simon@outlook.com>
2024-04-25 14:51:44 +0200

    Make interactive

--- src/bin/run_query.rs
+++ src/bin/run_query.rs
@@ -3,8 +3,9 @@ use arrow_array::StringArray;
 use async_openai::{
     config::OpenAIConfig,
     types::{
-        ChatCompletionRequestAssistantMessageArgs, ChatCompletionRequestSystemMessageArgs,
-        ChatCompletionRequestUserMessageArgs, CreateChatCompletionRequestArgs,
+        ChatCompletionRequestAssistantMessageArgs, ChatCompletionRequestMessage,
+        ChatCompletionRequestSystemMessageArgs, ChatCompletionRequestUserMessageArgs,
+        CreateChatCompletionRequestArgs,
     },
     Client,
 };
@@ -13,7 +14,7 @@ use fastembed::TextEmbedding;
 use futures::TryStreamExt;
 use lancedb::{query::ExecutableQuery, Table};
 use rag_rs::embed::init_model;
-use std::env;
+use std::{env, io::stdin};
 
 const TABLE_NAME: &str = "EmbeddingsTable";
 
@@ -32,22 +33,25 @@ async fn main() -> Result<()> {
         .with_api_base("http://localhost:8080/v1");
     let client = Client::with_config(local_conf);
 
+    // Create a thread for the conversation
+    let thread_request = CreateThreadRequestArgs::default().build()?;
+    let thread = client.threads().create(thread_request.clone()).await?;
+
     // Chat
     let system = ChatCompletionRequestSystemMessageArgs::default()
                 .content("Use the provided CONTEXT to answer questions. Documents in the CONTEXT are delimted with <--->. If the answer cannot be found in the CONTEXT, write 'I could not find an answer.'")
                 .build()?;
 
-    // TODO: This vec needs another type. The same one as the `.messages()` method
-    let mut msg_thread: Vec<String> = vec![system];
+    let mut msg_thread: Vec<ChatCompletionRequestMessage> = vec![system.into()];
 
     loop {
         // Read user message from stdin
         println!(">> Awaiting your message");
-        let mut user_msg = String::new();
-        let _ = stdin().read_line(&mut user_msg);
+        let mut query = String::new();
+        let _ = stdin().read_line(&mut query);
 
         // Retrieve neighbors
-        let nn_chunks = get_nearest_neighbor_chunks(query, &model, &tbl).await?;
+        let nn_chunks = get_nearest_neighbor_chunks(&query, &model, &tbl).await?;
         let context = nn_chunks[..2].join("\n<--->\n");
 
         let user_msg = ChatCompletionRequestUserMessageArgs::default()
@@ -58,11 +62,11 @@ async fn main() -> Result<()> {
             ))
             .build()?;
 
-        msg_thread.push(user_msg)
+        msg_thread.push(user_msg.into());
 
         let request = CreateChatCompletionRequestArgs::default()
             .n(1)
-            .messages(msg_thread)
+            .messages(msg_thread.clone())
             .build()
             .context("Failed to build ChatCompletionRequest")?;
 
@@ -70,19 +74,16 @@ async fn main() -> Result<()> {
             .chat()
             .create(request)
             .await
-            .context("Failed to create CompletionResponse")?;
-        println!("\nResponse:\n");
-        let response_text = response
+            .context("Failed to create CompletionResponse")?
             .choices
             .first()
             .unwrap()
-            .message
-            .content
-            .as_ref()
-            .unwrap();
+            .message;
+        msg_thread.push(response.into());
+        println!("\nResponse:\n");
+        let response_text = response.content.as_ref().unwrap();
         print!("{response_text}");
     }
-    //TODO: Chat with the user
     Ok(())
 }
 
